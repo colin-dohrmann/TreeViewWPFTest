c71fb68f53efaf06ce8965e35060360206e5a81a
colin-dohrmann <dohrmann.colin@googlemail.com>
2023-03-21 23:47:18 +0100

    Initial Commit

---
+++ Page.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeViewTest
+{
+    public class Page : ViewModelBase
+    {
+        private int _pageId;
+        private string _pageName;
+        [AllowNull] private ObservableCollection<Page> _subPages;
+        public int PageId
+        {
+            get { return _pageId; }
+            set { _pageId = value; OnPropertyChanged(nameof(PageId)); }
+        }
+        public string PageName
+        {
+            get { return _pageName; }
+            set { _pageName = value; OnPropertyChanged(nameof(PageName)); }
+        }
+
+        public ObservableCollection<Page> SubPages
+        {
+            get { return _subPages; }
+            set { _subPages = value; OnPropertyChanged(nameof(SubPages)); }
+        }
+
+        public Page(int pageId, string pageName, ObservableCollection<Page> subPages)
+        {
+            PageId = pageId;
+            PageName = pageName;
+            SubPages = subPages;
+        }
+        public Page(int pageId, string pageName)
+        {
+            PageId = pageId;
+            PageName = pageName;
+            _subPages = null;
+        }
+    }
+}
---
+++ TreeViewViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeViewTest
+{
+    public class TreeViewViewModel : ViewModelBase
+    {
+        public ObservableCollection<Page> Pages { get; set; }
+
+        public TreeViewViewModel()
+        {
+            Pages = new ObservableCollection<Page>
+            {
+                new Page(1, "Page 1"),
+                new Page(2, "Page 2"),
+                new Page(3, "Page 3"),
+                new Page(4, "Page 4"),
+            };
+            OnPropertyChanged(nameof(Pages));
+
+        }
+    }
+}
